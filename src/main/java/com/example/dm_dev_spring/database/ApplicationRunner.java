e94cbc488f630780d08c1a627df7e99bbfcfa3b9
Admin <abdutokt2004>
2023-03-01 12:21:15 +0600

    Add dependency JSR and create ApplicationConfiguration lesson 23, 24

--- src/main/java/com/example/dm_dev_spring/database/ApplicationRunner.java
+++ src/main/java/com/example/dm_dev_spring/database/ApplicationRunner.java
@@ -1,9 +1,11 @@
 package com.example.dm_dev_spring.database;
 
+import com.example.dm_dev_spring.config.ApplicationConfiguration;
 import com.example.dm_dev_spring.database.pool.ConnectionPool;
 import com.example.dm_dev_spring.database.repository.CompanyRepository;
 import com.example.dm_dev_spring.database.repository.CrudRepository;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.Serializable;
@@ -15,7 +17,7 @@ public class ApplicationRunner {
         System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
         System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
 
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
             var companyRepository = context.getBean("companyRepository", CrudRepository.class);

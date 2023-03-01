b645b04393fbe9888187bceb85349261e62cf437
Admin <abdutokt2004>
2023-03-01 12:31:09 +0600

    lesson 25 annotation Import

--- src/main/java/com/example/dm_dev_spring/config/ApplicationConfiguration.java
+++ src/main/java/com/example/dm_dev_spring/config/ApplicationConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.dm_dev_spring.config;
 
 import com.example.dm_dev_spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.example.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.example.dm_dev_spring",
